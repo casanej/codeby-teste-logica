7ccae4e664a959c787e7556106b74fccd08f6841
Rafael Casanje <rafael.casanje@gmail.com>
2021-03-06 01:55:08 -0300

    Finishing logic test

--- productStructure.js
+++ productStructure.js
@@ -14,5 +14,21 @@ const products = [
 ]
 
 module.exports = () => {
-  // Código aqui
+  const object = {}
+  
+  products.map(product => {
+    const item = product.split('-');
+
+    if(!Object.keys(object).includes(item[0])) {
+      object[item[0]] = {}
+    }
+
+    if(!Object.keys(object[item[0]]).includes(item[1])) {
+      object[item[0]][item[1]] = 0;
+    }
+
+    object[item[0]][item[1]] += 1;
+  })
+
+  return object;
 }
